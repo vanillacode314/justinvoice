5c640d7885fac03b2508a745fcd2b9c284bacbc9
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-02-10 04:16:56 +0530

    make fab have labels

--- src/components/Fab.svelte
+++ src/components/Fab.svelte
@@ -13,9 +13,9 @@
 </script>
 
 <div transition:scale={{ duration: 250 }} class="fixed bottom-0 right-0 lg:hidden p-5 z-30">
-	<div class="flex flex-col-reverse items-center gap-3">
+	<div class="flex flex-col-reverse items-end gap-3">
 		<button
-			class="p-5 grid place-content-center bg-stone-800 shadow-lg rounded-full transition hover:bg-stone-700 focus:bg-stone-700 preserve-3d"
+			class="p-5 grid place-content-center bg-stone-800 shadow-lg rounded-xl transition hover:bg-stone-700 focus:bg-stone-700 preserve-3d"
 			class:rotate-90={open}
 			class:bg-stone-700={open}
 			class:bg-stone-800={!open}
@@ -26,12 +26,13 @@
 		>
 			<span class="{open ? 'i-mdi-close' : 'i-mdi-menu'} text-xl" />
 		</button>
-		<div class="flex flex-col-reverse items-center gap-3">
+		<div class="flex flex-col-reverse gap-3">
 			{#each [...(open ? actions : [])].reverse() as { color, icon, label, action, noClose }, index (label)}
 				<button
+					type="button"
 					out:fly={{ duration: 100, delay: 50 * (actions.length - index), y: 10 * index }}
 					in:fly={{ duration: 100, delay: 50 * index, y: 10 * (actions.length - index) }}
-					class="btn btn-circle p-3 grid place-content-center {color} shadow-lg rounded-full transition-transform"
+					class="btn {color} shadow-lg rounded-xl transition-transform flex gap-3 items-center justify-start"
 					on:click={() => {
 						action()
 						if ($appState.selectionMode) return
@@ -41,6 +42,9 @@
 					}}
 				>
 					<span class="{icon} text-xl" />
+					<span>
+						{label}
+					</span>
 				</button>
 			{/each}
 		</div>
